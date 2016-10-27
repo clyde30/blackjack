0799c1a20dbfb9a716ccc34d3393b5b4ff02955b
Austin Jones <ajonestx@gmail.com>
2016-10-26 21:15:39 -0600

    deal two cards to player

--- src/js/deal.js
+++ src/js/deal.js
@@ -1,13 +1,13 @@
-function dealCard() {
+function dealCard(deck) {
 	var card = deck.shift();
 
 	return card;
 };
 
-function dealNewHand(numcards) {
+function dealNewHand(numcards, deck) {
 	var hand = [];
-	for (var i = 0; i <=numcards; i++) {
-		hand.push(dealCard())
+	for (var i = 0; i < numcards; i++) {
+		hand.push(dealCard(deck))
 	}
 	return hand;
 }
--- src/js/dealerApp.js
+++ src/js/dealerApp.js
@@ -5,16 +5,17 @@ angular.module('blackjackApp', [])
 
     self.shuffle = function () {
       var numDecks = self.numDecks;
-      var deck = shuffle(numDecks);
+      self.deck = shuffle(numDecks);
       // self.player1.hand = [];
       // self.dealer.hand = [];
       console.log("Shuffled " + numDecks + " decks");
-      console.log(deck);
     };
 
     self.initialDeal = function () {
+      console.log(self.deck);
+      var deck = self.deck;
       console.log("Dealing");
-      var player = new Player(dealNewHand(2));
+      var player = new Player(dealNewHand(2, deck));
       // var dealer = new Player();
       player.hand.push();
       // dealer.hand.push(dealCard());
