dc4345871a28a4ee35ddbd4e20bdb499652f1345
Austin Jones <ajonestx@gmail.com>
2016-12-24 00:28:56 -0700

    chore: update gulpfile to concat and minify js and less

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,44 @@
 var gulp = require('gulp'),
   connect = require('gulp-connect');
+  jshint = require('gulp-jshint');
+  less = require('gulp-less');
+  cleanCSS = require('gulp-clean-css');
+  uglify = require('gulp-uglify');
+  concat = require('gulp-concat');
+  rename = require('gulp-rename');
 
 gulp.task('webserver', function() {
   connect.server();
 });
 
-gulp.task('default', ['webserver']);
+gulp.task('less', function() {
+  return gulp.src('src/less/**/*.less')
+    .pipe(less())
+    .pipe(cleanCSS())
+    .pipe(concat('main.css'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('public/css'));
+});
+
+gulp.task('js', function () {
+  gulp.src('src/js/**/*.js')
+  .pipe(jshint())
+  .pipe(jshint.reporter('jshint-stylish'))
+  .pipe(uglify())
+  .pipe(concat('app.js'))
+  .pipe(rename({suffix: '.min'}))
+  .pipe(gulp.dest('public/js'))
+});
+
+gulp.task('jshint', function() {
+  return gulp.src('src/js/**/*.js')
+  .pipe(jshint())
+  .pipe(jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('src/js/**/*.js', ['js']);
+  gulp.watch('src/less/**/*.less', ['less']);
+});
+
+gulp.task('default', ['webserver', 'watch']);
